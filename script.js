538257feb65fbac43365772ff42e0a594ca746d7
Diego Fernando Mera Largo <cloudinthesky@outlook.es>
2022-03-10 11:55:22 -0500

    Final result

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 function computerPlay() {
+
     let options = ["Rock","Scissors","Paper"];
 
    const random = Math.floor(Math.random()*options.length);
@@ -11,28 +12,65 @@ function computerPlay() {
 
 
 
-//const playerSelection = prompt("Choose bteween: Rock, Paper and Scissors");
-//const computerSelection = computerPlay();
 
-function playRound(playerSelection, computerSelection){
+
+/*function playRound(playerSelection, computerSelection){
 
  
    let player= playerSelection.toLowerCase();
    let computer = computerSelection.toLowerCase();
+
+
+  
     
     if(player=='scissors'&&computer=='rock'||player=='paper'&&computer=='scissors'||player=='rock'&&computer=='paper'){
-        return `You Lose! ${computer} beats ${player}`;
+        
+        
+        return 'You Lose! ' + `${computer} beats ${player}` 
+
     }else if(player=='rock'&&computer=='scissors'||player=='scissors'&&computer=='paper'||player=='paper'&&computer=='rock'){
-        return `You won! ${player} beats ${computer}`;
+       
+        
+        return 'You won! ' + `${player} beats ${computer}` 
+
     }else if(player=='scissors'&&computer=='scissors'||player=='paper'&&computer=='paper'||player=='rock'&&computer=='rock'){
-        return 'DRAW!!!';
+        return 'DRAW!!!'
     }else{
         return 'sorry, those are not valid options';
     }
     
-  
+
+    
+}*/
+
+let userScore = 0;
+let compScore = 0;
+
+
+
+function playRound(userInput,compInput){
+
+   if(userInput=="scissors"&&compInput=="paper"||userInput=="rock"&&compInput=="scissors"||userInput=="paper"&&compInput=="rock"){
+       userScore++;
+       return `You won ${userInput} beats ${compInput}`;
+   }else if(compInput==userInput){
+       userScore++
+       compScore++
+       return `it's a draw`
+   }else if(compInput=="scissors"&userInput=="paper"||compInput=="rock"&&userInput=="scissors"||compInput=="paper"&&userInput=="rock"){
+       compScore++;
+       return `You lose... ${compInput} beats ${userInput}`
+   }else{
+       return 'sorry, thats an invalid option :c'
+   }
+    
+
+
+
+
     
 }
+
 
 
 
@@ -41,16 +79,34 @@ function playRound(playerSelection, computerSelection){
 function game(){
   
 
- 
+
     for (let i = 0; i < 5; i++) {
-        const playerSelection = prompt("Choose bteween: Rock, Paper and Scissors");
-        const computerSelection = computerPlay();
-        console.log(playRound(playerSelection, computerSelection));
-     
-    }
+
   
- 
+
+  let userInput = prompt("Choose between: Rock, Paper or Scissors").toLowerCase();
+    let compInput = computerPlay().toLowerCase();
+     
+       console.log(playRound(userInput,compInput));
+       
+       
+      
+
+    }
+    if(userScore>compScore){
+        console.log("You have won!")
+    }else{
+       console.log("you have lost")
+    }
+     
+
+
+
+  
 }
 
 
-game()
+game();
+
+
+
